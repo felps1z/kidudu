3f2ddf83e32dd9da2e6c35a91578cec2bd5aeb57
felps1z <felipesz180@gmail.com>
2024-06-29 10:55:32 -0300

    fixando barra de navegacao, adicionando animacoes com scrollreveal

--- src/script.js
+++ src/script.js
@@ -10,10 +10,40 @@ function closeMenu() {
     menuMobile.classList.add('hidden');
 }
 
-
-function showItems(){
+function showItems() {
     const elementItems = document.querySelector('#items');
     let items = 0;
     elementItems.innerHTML = items;
 }
+
 showItems();
+
+//Criação de um evento para sombra no header
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    const scrollPosition = window.scrollY - header.offsetHeight;
+
+    if (scrollPosition <= 0) {
+        header.style.boxShadow = 'none';
+    } else {
+        header.style.boxShadow = '5px 1px 5px rgba(0, 0, 0, 0.1)';
+    }
+});
+
+ScrollReveal().reveal('#quemsomos', {
+    origin: 'bottom',
+    duration: 1500,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('#loc', {
+    origin: 'left',
+    duration: 1500,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('#loc-title', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
