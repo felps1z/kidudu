98b613bfd5f78976dee0cebf61b6df2407956a2c
felps1z <felipesz180@gmail.com>
2024-07-05 07:22:14 -0300

    ajuste na estilização dos produtos, mudança de tipografia

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,14 @@ module.exports = {
         "pink-color": "#F9B4AF",
         "green-color": "#92DD92",
         "gray-text": "#5C5C5C",
-        "gray-border": "#909090",
+        "gray-border": "#B4B4B4",
         "gray-bg": "#F3F3F3",
         "success": "#42CA20",
         "danger": "#FF0000",
       },
-      fontFamily: { 
+      /*fontFamily: { 
         "sans": ["Inter", "sans-serif"]
-      }
+      }*/
     },
   },
   plugins: [],
