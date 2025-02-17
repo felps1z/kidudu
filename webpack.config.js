d49550d0497bd1c744d6bab66a343604202f1187
felps1z <felipesz180@gmail.com>
2025-02-17 14:20:12 -0300

    mudando para production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/assets/js/script.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
